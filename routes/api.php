d29a1a8b67c9ef19422ab8ecd89f196dbcff50c0
RatulSaqibKhan <ratulkhan.jhenidah@gmail.com>
2020-10-18 20:01:46 +0600

    API endpoint and User Model Updated according to JWT integration method

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,28 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+
+    'middleware' => 'api',
+    'namespace' => 'App\Http\Controllers',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::post('logout', 'AuthController@logout');
+    Route::get('profile', 'AuthController@profile');
+    Route::post('refresh', 'AuthController@refresh');
+
+});
+
+Route::group([
+
+    'middleware' => 'api',
+    'namespace' => 'App\Http\Controllers',
+
+], function ($router) {
+
+    Route::resource('todos', 'ToDoController');
 });
